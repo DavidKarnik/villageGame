912fdb78b1071b6f28995a24a2163975ac0e7cf5
David_Karnik <david.karnik@tul.cz>
2024-06-30 21:22:04 +0200

    Add png and positioning villageL

--- game-frontend/src/app/village-image/village-image.component.ts
+++ game-frontend/src/app/village-image/village-image.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-village-image',
   templateUrl: './village-image.component.html',
   styleUrls: ['./village-image.component.css']
 })
-export class VillageImageComponent {
+export class VillageImageComponent implements OnInit {
+  @Input() mainLevel: number = 1;
+  @Input() smithLevel: number = 1;
+  @Input() barracksLevel: number = 1;
 
+  villageImage = '';
+  mainImage = '';
+  smithImage = '';
+  barracksImage = '';
+
+  ngOnInit(): void {
+    this.updateImages();
+  }
+
+  updateImages(): void {
+    this.villageImage = `assets/village.png`;
+    this.mainImage = `assets/main${this.mainLevel}.png`;
+    this.smithImage = `assets/smith${this.smithLevel}.png`;
+    this.barracksImage = `assets/barracks${this.barracksLevel}.png`;
+
+    console.log('Village image:', this.villageImage);
+    console.log('Main image:', this.mainImage);
+    console.log('Smith image:', this.smithImage);
+    console.log('Barracks image:', this.barracksImage);
+  }
 }
